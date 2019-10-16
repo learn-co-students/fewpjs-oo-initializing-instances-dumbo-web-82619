754a88bff5f8a77cb7dc41d172c26496094d12b7
dangrammer <52326502+dangrammer@users.noreply.github.com>
2019-10-16 15:11:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food
+    this.drink = drink
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad
+    this.soup = soup
+    this.drink = drink
+  }
+}
+
+class Dinner {
+  constructor(salad, soup, entree, _dessert) {
+    this.salad = salad
+    this.soup = soup
+    this.entree = entree
+    this._dessert = _dessert
+  }
+}
+
+let brekkie = new Breakfast('eggs', 'orange juice')
+let lunch = new Lunch('caesar salad', 'tomato soup', 'water')
+let dindin = new Dinner('cobb salad', 'borscht', 'roast beef & kale', 'apple pie')
